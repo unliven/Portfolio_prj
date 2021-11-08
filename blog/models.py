3774825fa8909d886d95755b7988efa2bf9fb3f3
unliven <dolmat.vadim@gmail.com>
2021-11-08 22:37:41 +0300

    Add features to site

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
-    description = models.CharField(max_length=250)
-    url = models.URLField(blank=True)
+    description = models.TextField(max_length=400)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from .models import Blog
 
 
 def all_blogs(request):
-    blog = Blog.objects.all()
-    count = blog.count()
-    return render(request, 'blog/all_blogs.html', {'blogs': blog, 'blogs_count': count})
+    blog_obj = Blog.objects.all()
+    count = blog_obj.count()
+    return render(request, 'blog/all_blogs.html', {'blogs': blog_obj, 'blogs_count': count})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
